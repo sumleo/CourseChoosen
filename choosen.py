e0a6f3667e9299e69a83d10fa008795fd15168b1
sumleo <992252973@qq.com>
2017-09-17 13:24:09 +0800

    Create choosen.py

---
+++ choosen.py
@@ -0,0 +1,17 @@
+import requests
+headers={'Accept':'*/*',
+'Accept-Encoding':'gzip,deflate',
+'Accept-Language':'zh-Hans-CN, zh-Hans; q=0.5',
+'Connection':'Keep-Alive',
+#抓一下cookie
+'Cookie':'',
+'Host':'jwxt.sustc.edu.cn',
+'Referer':'http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/comeInFawxk',
+'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393',
+'X-Requested-With':'XMLHttpRequest'
+}
+payload={'jx0404id':'选课票据','trjf':'','xkzy':''}
+#选课api
+request= requests.get(url='http://jwxt.sustc.edu.cn/jsxsd/xsxkkc/fawxkOper',params=payload,headers=headers)
+print(request.text)
+
